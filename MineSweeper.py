e5575a901032139c0f30b2a84481a11531b046ad
Samet@VH <s1cakposta@hotmail.com>
2022-02-07 15:25:41 +0100

    Redo of the first version-top down approach without OOP

--- MineSweeper.py
+++ MineSweeper.py
@@ -5,39 +5,24 @@ First a welcome screen with a prompt to input the grid size and amount of mines
 A 2D integer list to store the grid
 A 2D boolean list to store the cells if there is a bomb
 
+start_game:
+    get_grid_input
+play_game:
+    get_user_input
+    check if already selected
+    check if there is a mine
+        if there is a mine:
+            update_grid
+            show_grid
+            show_game_over
+            ask_to_play_again
+        else:
+            update_grid
+            show_grid
+
 """
+import random
 
 
-class Game:
-    def __init__(self, game_screen=None, mines=0):
-        self.game_screen = game_screen
-        self.mines = mines
-
-    def play_game(self):
-        self.reset_game()
-        self.game_screen = Screen()
-        self.game_screen.get_user_input()
-
-    def reset_game(self):
-        self.game_screen = None
-
-
-class Screen:
-
-    def __init__(self, num_row=0, num_col=0, num_mines=0):
-        self.num_row = num_row
-        self.num_col = num_col
-        self.num_mines = num_mines
-
-    def get_user_input(self):
-        self.num_row, self.num_col, self.num_mines = input(
-            "Enter grid size as 'row' 'col' and the number of mines with spaces in "
-            "between [ex: 4 6 7]: ").split()
-        print("Grid: " + str(self.num_row) + "x" + str(self.num_col) + " mines: " + str(self.num_mines))
-
-    def place_mines(self):
-        pass
-
-
-new_game = Game()
-new_game.play_game()
+def draw_grid(grid_size):
+    pass
